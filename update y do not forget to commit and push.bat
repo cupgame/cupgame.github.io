484154fc875d7b16db4451da8446155774ea3c8d
harrisontree <harrisontree@users.noreply.github.com>
2018-05-10 08:56:54 -0700

    re- organized files

---
+++ update y do not forget to commit and push.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo we will now copy files to drive y, don't forget to use git to commit AND push
+echo.
+pause
+
+xcopy .  y:\ddccode1\assignment4\finalproject /s /y
+
+echo.
+echo done
+echo.
+pause
